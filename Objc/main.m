7abc1bb166158588825aa4e56a19009783ca2517
therealniks <therealniks@gmail.com>
2021-05-18 00:29:47 +0300

    Initial Commit

---
+++ Objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objc
+//
+//  Created by Nikita on 18.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
