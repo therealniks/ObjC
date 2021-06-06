62950572290703bacc46db6f33b1bb94ddeb52d0
therealniks <therealniks@gmail.com>
2021-06-06 19:19:08 +0300

    with blocks

--- Objc/main.m
+++ Objc/main.m
@@ -6,21 +6,22 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Student.h"
+#import "Arithmetic.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableArray *students = [NSMutableArray array];
-        Student *studentII = [[Student alloc] initWithName:@"Ivan" Surname:@"Ivanov" andAge:@22];
-        Student *studentPP = [[Student alloc] initWithName:@"Petr" Surname:@"Petrov" andAge:@23];
-        Student *studentSS = [[Student alloc] initWithName:@"Sidor" Surname:@"Sidorov" andAge:@24];
-        Student *student = [Student new];
-        [students addObject:studentII];
-        [students addObject:studentPP];
-        [students addObject:studentSS];
-        NSLog(@"%lu", (unsigned long)students.count);
-        [student addOneYear];
-        NSLog(@"%@", students.description);
+        int first = 10;
+        int second = 2;
+        int resultSum = [Arithmetic beginWithAction:ActionTypeSum firstNumber:first secondNumber:second];
+        int resultSubstraction = [Arithmetic beginWithAction:ActionTypeSubstraction firstNumber:first secondNumber:second];
+        int resultMultiplication = [Arithmetic beginWithAction:ActionTypeMultiplication firstNumber:first secondNumber:second];
+        int resultDivision = [Arithmetic beginWithAction:ActionTypeDivision firstNumber:first secondNumber:second];
+        int resultRemainderOfTheDivision = [Arithmetic beginWithAction:ActionTypeRemainderOfTheDivision firstNumber:first secondNumber:second];
+        NSLog(@"Result sum - %i", resultSum);
+        NSLog(@"Result substraction - %i", resultSubstraction);
+        NSLog(@"Result multiplication - %i", resultMultiplication);
+        NSLog(@"Result division - %i", resultDivision);
+        NSLog(@"Result remainder of the division - %i", resultRemainderOfTheDivision);
     }
     return 0;
 }
